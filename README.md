e4f925df683aead21dfa36e7bfb92727bb2d07d4
Ace <ace.github@gmail.com>
2014-07-14 13:07:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# EgoPost
+## Ensuring the world gets the opportunity to see absolutely everything you eat, think or feel
+
+This is a basic Twitter clone created with Ruby on Rails and LESS.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Egopost::Application.config.secret_token = '90a5ab321f047a50ea9b9f1d088d79d5ec9a3f2a20219751ddd06a95c72f7edd5f1edcfb54d507dc82dd000bcac69e49ea5aab83258bbfc9962b2e04b8c22c5e'
